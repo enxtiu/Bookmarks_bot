eb216f550d63c632133b37297deb91d466994322
enxtiu <kuznetsov.fedo@yandex.ru>
2024-12-09 16:09:05 +0700

    creating: databases

--- app/bot/databases/database.py
+++ app/bot/databases/database.py
@@ -1,6 +1,16 @@
-def main():
-    pass
+import logging
+
+logger = logging.getLogger(__name__)
+
+class User:
+
+    def __init__(self, _id_user: int, _number_page: int) -> None:
+        self._id_user = _id_user
+        self._number_page = _number_page
 
 
-if __name__ == '__main__':
-    pass
+
+class BookMarks(User):
+
+    def __init__(self, _id_user: int, number_page: int, mark: str) -> None:
+        super().__init__(_id_user, number_page)
