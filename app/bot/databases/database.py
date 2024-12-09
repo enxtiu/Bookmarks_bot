32abe5a2998f55a3969e7f99d4f2fb7c2d39113b
enxtiu <kuznetsov.fedo@yandex.ru>
2024-12-09 22:55:16 +0700

    modified: databaases init workflow_data

--- app/bot/databases/database.py
+++ app/bot/databases/database.py
@@ -1,16 +1,22 @@
 import logging
 
+from app.bot.lexicons.lexicon import BUTTONS
 logger = logging.getLogger(__name__)
 
 class User:
 
-    def __init__(self, _id_user: int, _number_page: int) -> None:
-        self._id_user = _id_user
-        self._number_page = _number_page
+    def __init__(self, number_page: int, mark: list[str, str] | None) -> None:
+        self.number_page = number_page
+        self.mark = mark
 
+    def get_mark(self) -> list[str, str]:
+        return self.mark
 
+    def next_page(self) -> None:
+        if len(BUTTONS) != self.number_page:
+            self.number_page += 1
 
-class BookMarks(User):
+    def back_page(self) -> None:
+        if self.number_page != 1:
+            self.number_page -= 1
 
-    def __init__(self, _id_user: int, number_page: int, mark: str) -> None:
-        super().__init__(_id_user, number_page)
--- app/bot/tg_bot.py
+++ app/bot/tg_bot.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher
 
 from app.bot.configs.config_data import load_config, Config
+from app.bot.databases.database import User
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +14,7 @@ async def main() -> None:
     logger.info('init bot, dp')
     bot = Bot(config.tg_bot.token)
     dp = Dispatcher()
+    dp.workflow_data.update({'user': User(1, None)})
 
 
     await bot.delete_webhook(drop_pending_updates=True)
