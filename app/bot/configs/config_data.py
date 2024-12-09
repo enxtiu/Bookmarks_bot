d62c6b973c4f198fb0002e881c693b18263c24cf
enxtiu <kuznetsov.fedo@yandex.ru>
2024-12-09 14:37:10 +0700

    update_conflict_creating: configs/config_data init token for tg_bot

--- app/bot/configs/config_data.py
+++ app/bot/configs/config_data.py
@@ -1,1 +1,25 @@
+import logging
 
+from dataclasses import dataclass
+
+
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+logger = logging.getLogger(__name__)
+
+
+def load_config() -> Config:
+    import os
+    from dotenv import load_dotenv, find_dotenv
+    logger.info('load .env')
+    load_dotenv(find_dotenv())
+    logger.info('return Config')
+    return Config(tg_bot=TgBot(token=os.getenv('TOKEN')))
