47112667fda1a1dd47ee71649b8a15e6a8b0885e
enxtiu <kuznetsov.fedo@yandex.ru>
2024-12-09 15:30:54 +0700

    creating: lexicon

---
+++ app/bot/lexicons/lexicon.py
@@ -0,0 +1,8 @@
+with open(r'C:\Users\Redmi\PycharmProjects\Bookmarks\book.txt', encoding='utf-8', errors='replace') as file:
+   file = file.readlines()
+   print(file)
+
+
+NAME_1 = file[0]
+NAME_2 = file[1]
+BUTTONS = {str(k):v.rstrip() for k, v in enumerate(file[2:], start=2)}
