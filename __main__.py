62e29611d9717e636e2df930b18c124ebb5f831b
enxtiu <kuznetsov.fedo@yandex.ru>
2024-12-09 14:43:25 +0700

    creating: __main__

--- __main__.py
+++ __main__.py
@@ -1,5 +1,9 @@
-def main():
-    pass
+import logging.config, yaml
+
+with open('app/logging_config/config_logging.yaml', "tr") as file:
+    config = yaml.safe_load(file.read())
+    logging.config.dictConfig(config)
+
 
 
 if __name__ == '__main__':
